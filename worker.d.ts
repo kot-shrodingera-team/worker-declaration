039294eb2517f49c2f1f0a1d5f45078403f737f2
kot-shrodingera <kot.shrodingera@gmail.com>
2021-01-26 23:28:32 +0600

    Обновление до v1.0.8

--- worker.d.ts
+++ worker.d.ts
@@ -305,11 +305,11 @@ interface GermesWorker {
      * Параметр ставки (для фор, тоталов и тд)
      */
     Parametr: number;
-    /**
-     * Параметры воркера для этой БК (в JSON формате)
-     */
-    WorkerParameters: string;
   };
+  /**
+   * Параметры воркера для этой БК (в JSON формате)
+   */
+  WorkerParameters: string;
 }
 
 declare global {
